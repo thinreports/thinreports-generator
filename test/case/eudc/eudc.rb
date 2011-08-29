8a251cfc95a5c579f9186e4391545f83344f1472
hidakatsuya <work.hidaka@gmail.com>
2011-08-29 03:11:37 +0000

    Testing with bold/italic refs #227
    git-svn-id: http://osc-repos.matsukei.net/thinreports-generator/trunk@75 70f974f0-580b-4484-b989-98b15432365d

--- test/case/eudc/eudc.rb
+++ test/case/eudc/eudc.rb
@@ -12,6 +12,9 @@ ThinReports::Report.generate_file(:pdf, CaseRunner.output_file) do
   start_new_page
   
   page.item(:eudc).value('日本で生まれ世界が育てた言語 Ruby')
+  page.values(:eudc_bold        => '太字',
+              :eudc_italic      => '斜体',
+              :eudc_bold_italic => '太字斜体')
 end
 
 ThinReports.config.generator.pdf.eudc_ttf = []
