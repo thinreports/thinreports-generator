6e32208d91748c29c8b030c497a301755ebe7101
Katsuya Hidaka <hidakatsuya@gmail.com>
2015-04-17 02:56:39 +0900

    Improved comment message

--- examples/report_callbacks/report_callbacks.rb
+++ examples/report_callbacks/report_callbacks.rb
@@ -16,12 +16,11 @@ example :report_callbacks, 'Callbacks of Report' do |t|
   report.start_new_page
   report.start_new_page
 
-  # on_generate callback in 0.7.7 or lower
+  # A handler called before finalizing report in 0.7.7 or lower
   report.events.on :generate do |e|
-    e.pages.each {|page| page.item(:text3).value('generate event in 0.7.7 or lower') }
+    e.pages.each {|page| page.item(:text3).value('A handler called before finalizing report in 0.7.7 or lower')
   end
 
-  # Instead of on_generate callback in 0.8 or higher
   report.pages.each do |page|
     page.item(:text4).value('emulation of generate event in 0.8 or higher')
   end
