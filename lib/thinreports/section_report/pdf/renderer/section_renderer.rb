3e1ba095f3f969955db876dea408c5ca14cf01af
sunflat <git.sunflat@sunflat.net>
2018-02-21 15:03:40 +0900

    detailsを描画

--- lib/thinreports/section_report/pdf/renderer/section_renderer.rb
+++ lib/thinreports/section_report/pdf/renderer/section_renderer.rb
@@ -2,27 +2,35 @@ module Thinreports
   module SectionReport
     module Renderer
       class SectionRenderer
-        def initialize(pdf, section)
+        def initialize(pdf)
           @pdf = pdf
-          @section = section
         end
 
         def render(section)
           doc = pdf.pdf
 
-          box = doc.bounding_box([0, doc.cursor], width: 300) do
+          box = doc.bounding_box([0, doc.cursor], width: 300, height: section.schema.height) do
             section.items.each do |item|
-              pdf.draw_shape_rect(item.internal)
+              draw_item(item)
             end
-            doc.stroke_bounds
+            # doc.stroke_bounds
           end
-          # box.move_past_bottom
-          doc.move_down [section.schema.height, box.height].max
         end
 
         private
 
         attr_reader :pdf
+
+        def draw_item(item)
+          shape = item.internal
+          if shape.type_of?(Core::Shape::TextBlock::TYPE_NAME)
+            @pdf.draw_shape_tblock(shape)
+          elsif shape.type_of?('line')
+            @pdf.draw_shape_line(shape)
+          else
+            puts 'unknown shape type'
+          end
+        end
       end
     end
   end
