fc9acd827d1d484cd91345ab70635cda1b3ffb32
sunflat <git.sunflat@sunflat.net>
2018-02-21 16:42:45 +0900

    改ページ

--- lib/thinreports/section_report/pdf/renderer/group_renderer.rb
+++ lib/thinreports/section_report/pdf/renderer/group_renderer.rb
@@ -12,18 +12,55 @@ module Thinreports
         end
 
         def render(group)
-          pdf.pdf.start_new_page
+          doc = pdf.pdf
+          @page_count = 0
+
+
+          page_footers_size = 0
+          group.footers.each do |footer|
+            page_footers_size += footer.schema.height if footer.schema.every_page?
+          end
+          max_page_height = doc.bounds.height - page_footers_size
+
+
+          doc.start_new_page
+          @page_count += 1
+          current_page_height = 0
 
           group.headers.each do |header|
-            section_renderer.render(header) # if header.schema.every_page? || @page_count == 1
+            if header.schema.every_page? || @page_count == 1
+              section_renderer.render(header)
+              current_page_height += header.schema.height
+            end
           end
 
           group.details.each do |detail|
+            if current_page_height + detail.schema.height > max_page_height
+              group.footers.each do |footer|
+                section_renderer.render(footer) if footer.schema.every_page? && !footer.schema.fixed_bottom?
+              end
+
+              group.footers.each do |footer|
+                section_renderer.render(footer) if footer.schema.every_page? && footer.schema.fixed_bottom?
+              end
+
+              doc.start_new_page
+              @page_count += 1
+              current_page_height = 0
+
+              group.headers.each do |header|
+                if header.schema.every_page? || @page_count == 1
+                  section_renderer.render(header)
+                  current_page_height += header.schema.height
+                end
+              end
+            end
             section_renderer.render(detail)
+            current_page_height += detail.schema.height
           end
 
           group.footers.each do |footer|
-            section_renderer.render(footer) unless footer.schema.fixed_bottom?
+            section_renderer.render(footer) if !footer.schema.fixed_bottom?
           end
 
           group.footers.each do |footer|
