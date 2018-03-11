a07a3860249e092e4d980f766ba9f594ff6b9ce5
Katsuya HIDAKA <hidakatsuya@gmail.com>
2018-03-11 18:08:54 +0900

    Add configuration for registering font

--- test/unit/test_config.rb
+++ test/unit/test_config.rb
@@ -33,4 +33,46 @@ class Thinreports::TestConfig < Minitest::Test
     config.fallback_fonts.unshift 'Times New Roman'
     assert_equal config.fallback_fonts, ['Times New Roman', 'Helvetica', 'IPAMincho']
   end
+
+  def test_register_font
+    config = Thinreports::Configuration.new
+
+    assert_equal({}, config.fonts)
+
+    config.register_font('Foo',
+      normal: 'foo.ttf',
+      bold: 'foo_bold.ttf',
+      italic: 'foo_italic.ttf',
+      bold_italic: 'foo_bold_italic.ttf'
+    )
+    assert_equal({
+      normal: 'foo.ttf',
+      bold: 'foo_bold.ttf',
+      italic: 'foo_italic.ttf',
+      bold_italic: 'foo_bold_italic.ttf'
+    }, config.fonts['Foo'])
+
+    config.register_font('Bar', normal: 'bar.ttf')
+    assert_equal({
+      normal: 'bar.ttf',
+      bold: 'bar.ttf',
+      italic: 'bar.ttf',
+      bold_italic: 'bar.ttf'
+    }, config.fonts['Bar'])
+
+    assert_equal({
+      'Foo' => {
+        normal: 'foo.ttf',
+        bold: 'foo_bold.ttf',
+        italic: 'foo_italic.ttf',
+        bold_italic: 'foo_bold_italic.ttf'
+      },
+      'Bar' => {
+        normal: 'bar.ttf',
+        bold: 'bar.ttf',
+        italic: 'bar.ttf',
+        bold_italic: 'bar.ttf'
+      }
+    }, config.fonts)
+  end
 end
