42c3404a4b4c38646a8259164d5457969adf4700
sunflat <git.sunflat@sunflat.net>
2020-06-15 13:12:22 +0900

    Add offset-x and y properties to style parameters
    Co-authored-by: kosappi <kosaka.ryuichi@misoca.jp>
    Co-authored-by: Katsuya HIDAKA <hidakatsuya@gmail.com>

--- lib/thinreports/core/shape/style/basic.rb
+++ lib/thinreports/core/shape/style/basic.rb
@@ -5,8 +5,11 @@ module Thinreports
     module Shape
       module Style
         class Basic < Style::Base
-          style_accessible :visible
+          style_accessible :visible, :offset_x, :offset_y
           attr_accessor :visible
+
+          style_accessor :offset_x, 'offset-x'
+          style_accessor :offset_y, 'offset-y'
 
           def initialize(*args)
             super
