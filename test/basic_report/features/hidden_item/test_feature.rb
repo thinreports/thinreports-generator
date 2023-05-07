c81881761489014b31461efbbb2ba6b36adaac7c
Katsuya Hidaka <hidakatsuya@gmail.com>
2023-05-07 15:49:15 +0900

    all feature tests run without GC

--- test/basic_report/features/hidden_item/test_feature.rb
+++ test/basic_report/features/hidden_item/test_feature.rb
@@ -7,18 +7,6 @@ class Thinreports::BasicReport::TestHiddenItemFeature < Thinreports::FeatureTest
     report = Thinreports::BasicReport::Report.new layout: template_path
     2.times { report.list(:List).add_row }
 
-    # In CI (GitHub Actions), the following error occurs rarely.
-    #
-    # > Error:.
-    # > Thinreports::BasicReport::TestHiddenItemFeature#test_feature:.
-    # > Errno::ENOENT: No such file or directory @ apply2files
-    #
-    # Probably GC related. It is not a fundamental solution,
-    # but for now, the problem is reproduced only by CI, so we will deal with it by disabling GC.
-    GC.disable
-
     assert_pdf report.generate
-  ensure
-    GC.enable
   end
 end
